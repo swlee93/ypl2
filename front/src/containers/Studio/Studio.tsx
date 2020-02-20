c3b68874e5015b83a9899fcc77f3afaf1af2d9e3
swlee <swlee@whatap.io>
2020-02-20 21:13:14 +0900

    set NODE_PATH

--- front/src/containers/Studio/Studio.tsx
+++ front/src/containers/Studio/Studio.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Container } from '../../styles'
+import { Container } from 'styles'
 const Studio = (props) => {
-	return <div>studio</div>
+	return <Container>studio</Container>
 }
 export default Studio
